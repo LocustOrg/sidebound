36ac1a71d217a2ccb3bf6153fdba6cfa1dd4e849
Tarasikee <taras@deepinsight.io>
2026-05-22 23:53:10 +0200

    Initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        ignores: ['dist/'],
+    },
+)
+
