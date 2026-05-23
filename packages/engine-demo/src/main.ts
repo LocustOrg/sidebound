6b280447d7eb18f8786c37ff132fbe00ce8854bf
Tarasikee <taras@deepinsight.io>
2026-05-23 23:31:01 +0200

    Better scaling

--- packages/engine-demo/src/main.ts
+++ packages/engine-demo/src/main.ts
@@ -72,7 +72,7 @@ const engine = new PixelEngine({
     canvas,
     width: viewport.width,
     height: viewport.height,
-    scale: 6,
+    scale: 'css',
     background: '#1e1a2e',
     loop: {
         update(deltaSeconds) {
